cdf0c358a8beaec8e3489fbb40095f021ffc5df3
Homestead <vagrant@homestead.com>
2018-09-10 18:22:13 +0000

    removing unecessary js

--- public/js/app.js
+++ public/js/app.js
@@ -12968,7 +12968,7 @@ Popper.Defaults = Defaults;
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(3);
-module.exports = __webpack_require__(12);
+module.exports = __webpack_require__(13);
 
 
 /***/ }),
@@ -12976,7 +12976,6 @@ module.exports = __webpack_require__(12);
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(4);
-__webpack_require__(11);
 
 /***/ }),
 /* 4 */
@@ -12991,11 +12990,11 @@ try {
 
     __webpack_require__(7);
     __webpack_require__(8);
-    __webpack_require__(31);
     __webpack_require__(9);
-    __webpack_require__(30);
+    __webpack_require__(10);
+    __webpack_require__(11);
     //require('bootstrap-sweetalert');
-    window.swal = __webpack_require__(10);
+    window.swal = __webpack_require__(12);
 } catch (e) {}
 
 /***/ }),
@@ -25945,6 +25944,14 @@ S2.define('jquery.select2',[
 
 /***/ }),
 /* 9 */
+/***/ (function(module, exports) {
+
+/*! Select2 4.0.6-rc.1 | https://github.com/select2/select2/blob/master/LICENSE.md */
+
+(function(){if(jQuery&&jQuery.fn&&jQuery.fn.select2&&jQuery.fn.select2.amd)var e=jQuery.fn.select2.amd;return e.define("select2/i18n/es",[],function(){return{errorLoading:function(){return"No se pudieron cargar los resultados"},inputTooLong:function(e){var t=e.input.length-e.maximum,n="Por favor, elimine "+t+" car";return t==1?n+="ácter":n+="acteres",n},inputTooShort:function(e){var t=e.minimum-e.input.length,n="Por favor, introduzca "+t+" car";return t==1?n+="ácter":n+="acteres",n},loadingMore:function(){return"Cargando más resultados…"},maximumSelected:function(e){var t="Sólo puede seleccionar "+e.maximum+" elemento";return e.maximum!=1&&(t+="s"),t},noResults:function(){return"No se encontraron resultados"},searching:function(){return"Buscando…"}}}),{define:e.define,require:e.require}})();
+
+/***/ }),
+/* 10 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -27988,7 +27995,13 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 
 
 /***/ }),
-/* 10 */
+/* 11 */
+/***/ (function(module, exports) {
+
+!function(a){a.fn.datepicker.dates.es={days:["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"],daysShort:["Dom","Lun","Mar","Mié","Jue","Vie","Sáb"],daysMin:["Do","Lu","Ma","Mi","Ju","Vi","Sa"],months:["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],monthsShort:["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"],today:"Hoy",monthsTitle:"Meses",clear:"Borrar",weekStart:1,format:"dd/mm/yyyy"}}(jQuery);
+
+/***/ }),
+/* 12 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /*!
@@ -31467,180 +31480,10 @@ if (typeof window !== 'undefined' && window.Sweetalert2){  window.swal = window.
 "            transform: rotate(360deg); } }");
 
 /***/ }),
-/* 11 */
-/***/ (function(module, exports) {
-
-$(function () {
-  'use strict';
-
-  /////////////////// START: TEMPLATE SETTINGS /////////////////////
-
-  var loc = window.location.pathname;
-  var path = loc.split('/');
-  var isRtl = path[2].indexOf('rtl') >= 0 ? true : false;
-  var newloc = '';
-
-  // inject additional link tag for header skin
-  $('head').append('<link id="headerSkin" rel="stylesheet" href="">');
-
-  // inject template options content
-  /* $.get('../settings.html', function(data){
-       $('body').append(data);
-        
-       // set direction value in settings
-       if(isRtl) {
-         $('.slim-direction[value="rtl"]').prop('checked', true);
-       } else {
-         $('.slim-direction[value="ltr"]').prop('checked', true);
-       }
-        //check if header set to sticky
-       if($.cookie('sticky-header')) {
-         $('body').addClass('slim-sticky-header');
-         $('.sticky-header[value="yes"]').prop('checked', true);
-       } else {
-         $('.sticky-header[value="no"]').prop('checked', true);
-       }
-        //check if header have skin
-       if($.cookie('header-skin')) {
-         var sk = $.cookie('header-skin');
-         $('body').addClass(sk);
-         $('#headerSkin').attr('href',  '../css/slim.'+sk+'.css');
-         $('.header-skin[value="'+sk+'"]').prop('checked', true);
-       } else {
-         $('.header-skin[value="default"]').prop('checked', true);
-       }
-        //check if page set to wide
-       if($.cookie('full-width')) {
-         $('body').addClass('slim-full-width');
-         $('.full-width[value="yes"]').prop('checked', true);
-       } else {
-         $('.full-width[value="no"]').prop('checked', true);
-       }
-   });*/
-
-  // show/hide template options panel
-  $('body').on('click', '.template-options-btn', function (e) {
-    e.preventDefault();
-    $('.template-options-wrapper').toggleClass('show');
-  });
-
-  // set current page to light mode
-  $('body').on('click', '.skin-light-mode', function (e) {
-    e.preventDefault();
-    newloc = loc.replace('template-dark', 'template');
-    $(location).attr('href', newloc);
-  });
-
-  // set current page to dark mode
-  $('body').on('click', '.skin-dark-mode', function (e) {
-    e.preventDefault();
-    if (loc.indexOf('template-dark') >= 0) {
-      newloc = loc;
-    } else {
-      newloc = loc.replace('template', 'template-dark');
-    }
-    $(location).attr('href', newloc);
-  });
-
-  // set current page to rtl/ltr direction
-  $('body').on('click', '.slim-direction', function () {
-    var val = $(this).val();
-
-    if (val === 'rtl') {
-      if (!isRtl) {
-        if (path[3]) {
-          newloc = '/slim/' + path[2] + '-rtl/' + path[3];
-        } else {
-          newloc = '/slim/' + path[2] + '-rtl/';
-        }
-        $(location).attr('href', newloc);
-      }
-    } else {
-      if (isRtl) {
-        if (path[3]) {
-          newloc = '/slim/' + path[2].replace('-rtl', '') + '/' + path[3];
-        } else {
-          newloc = '/slim/' + path[2].replace('-rtl', '') + '/';
-        }
-        $(location).attr('href', newloc);
-      }
-    }
-  });
-
-  // toggles header to sticky
-  $('body').on('click', '.sticky-header', function () {
-    var val = $(this).val();
-    if (val === 'yes') {
-      $.cookie('sticky-header', 'true');
-      $('body').addClass('slim-sticky-header');
-    } else {
-      $.removeCookie('sticky-header');
-      $('body').removeClass('slim-sticky-header');
-    }
-  });
-
-  // set skin to header
-  $('body').on('click', '.header-skin', function () {
-    var val = $(this).val();
-    if (val !== 'default') {
-      $.cookie('header-skin', val);
-      $('#headerSkin').attr('href', '../css/slim.' + val + '.css');
-    } else {
-      $.removeCookie('header-skin');
-      $('#headerSkin').attr('href', '');
-    }
-  });
-
-  // set page to wide
-  $('body').on('click', '.full-width', function () {
-    var val = $(this).val();
-    if (val === 'yes') {
-      $.cookie('full-width', 'true');
-      $('body').addClass('slim-full-width');
-    } else {
-      $.removeCookie('full-width');
-      $('body').removeClass('slim-full-width');
-    }
-  });
-  /////////////////// END: TEMPLATE SETTINGS /////////////////////
-});
-
-/***/ }),
-/* 12 */
+/* 13 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 13 */,
-/* 14 */,
-/* 15 */,
-/* 16 */,
-/* 17 */,
-/* 18 */,
-/* 19 */,
-/* 20 */,
-/* 21 */,
-/* 22 */,
-/* 23 */,
-/* 24 */,
-/* 25 */,
-/* 26 */,
-/* 27 */,
-/* 28 */,
-/* 29 */,
-/* 30 */
-/***/ (function(module, exports) {
-
-!function(a){a.fn.datepicker.dates.es={days:["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"],daysShort:["Dom","Lun","Mar","Mié","Jue","Vie","Sáb"],daysMin:["Do","Lu","Ma","Mi","Ju","Vi","Sa"],months:["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],monthsShort:["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"],today:"Hoy",monthsTitle:"Meses",clear:"Borrar",weekStart:1,format:"dd/mm/yyyy"}}(jQuery);
-
-/***/ }),
-/* 31 */
-/***/ (function(module, exports) {
-
-/*! Select2 4.0.6-rc.1 | https://github.com/select2/select2/blob/master/LICENSE.md */
-
-(function(){if(jQuery&&jQuery.fn&&jQuery.fn.select2&&jQuery.fn.select2.amd)var e=jQuery.fn.select2.amd;return e.define("select2/i18n/es",[],function(){return{errorLoading:function(){return"No se pudieron cargar los resultados"},inputTooLong:function(e){var t=e.input.length-e.maximum,n="Por favor, elimine "+t+" car";return t==1?n+="ácter":n+="acteres",n},inputTooShort:function(e){var t=e.minimum-e.input.length,n="Por favor, introduzca "+t+" car";return t==1?n+="ácter":n+="acteres",n},loadingMore:function(){return"Cargando más resultados…"},maximumSelected:function(e){var t="Sólo puede seleccionar "+e.maximum+" elemento";return e.maximum!=1&&(t+="s"),t},noResults:function(){return"No se encontraron resultados"},searching:function(){return"Buscando…"}}}),{define:e.define,require:e.require}})();
 
 /***/ })
 /******/ ]);

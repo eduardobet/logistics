15ff698ecd0af8eb2ea7dd88eca8c44727b0733d
Josué HP Vagrant <josue.artaud@gmail.com>
2019-03-03 21:19:07 +0000

    Adding positions for tenant 2

--- app/DB/User.php
+++ app/DB/User.php
@@ -111,7 +111,7 @@ class User extends Authenticatable
 
     public function isSuperAdmin()
     {
-        return in_array($this->position, [1, 2]);
+        return in_array($this->position, [1, 2, 8, 9]);
     }
 
     /**
